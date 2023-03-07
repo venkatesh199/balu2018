0752ad2cfc7941fb2f5a51e9c7acaddc9c818358
Balu1479 <balu0000008@gmail.com>
2023-03-07 10:04:02 +0530

    Changes in perent to child components

--- force-app/main/default/lwc/childComponent/childComponent.js
+++ force-app/main/default/lwc/childComponent/childComponent.js
@@ -2,6 +2,13 @@ import { LightningElement,api } from 'lwc';
 
 export default class ChildComponent extends LightningElement {
     @api message;
+    constructor(){
+        super();
+        console.log('I am fron child component consructor');
+    }
+    connectedCallback(){
+        console.log('I am fron child component connected call back')
+    }
     @api
     handleChild(name){
         console.log('name from parent',name);
--- force-app/main/default/lwc/parentComponent/parentComponent.js
+++ force-app/main/default/lwc/parentComponent/parentComponent.js
@@ -1,7 +1,38 @@
 import { api, LightningElement } from 'lwc';
+import LANG from '@salesforce/i18n/lang';
+import DIR from '@salesforce/i18n/dir';
 
 export default class ParentComponent extends LightningElement {
+    lang = LANG;
+    dir = DIR;
     @api parentMessage;
+    name;
+    rajee;
+    renderValue;
+    hasRendered  = true;
+    constructor(){
+        super();
+        this.name = 'Moksha Sree Bala';
+        console.log('I am from parent component constructor');
+    }
+    connectedCallback(){
+        console.log('I am from parent component conected call back');
+        this.rajee = 'Moksha Sree Bala';
+        console.log('Rajee name--:',this.rajee);
+        console.log('language name--:',this.lang);
+        console.log('dir name--:',this.dir);
+    }
+    connectedCallback(){
+        this.rajee = 'Moksha Sree Bala Gongolla';
+        console.log('Rajee name--:',this.rajee);
+    }
+    renderedCallback(){
+        if(this.hasRendered){
+            this.renderValue = 'this is the rendered call back value';
+            this.hasRendered = false;
+        }
+        
+    }
     handleClick(){
         this.parentMessage = 'Moksha Sree';
         console.log('message',this.parentMessage);
@@ -13,8 +44,12 @@ export default class ParentComponent extends LightningElement {
         console.log('key--',key);
         console.log('value--',value);
         this.parentMessage = key +' '+value;
+        this.name = this.template.querySelector(".moksha").value;
+        console.log('Name--:',this.name);
         console.log('this.parentMessage--',this.parentMessage);
-
     }
-
+    handleRenderButton(){
+        this.renderValue = 'This is the button clicked value';
+        console.log('button value--:',this.renderValue);
+    }
 }
